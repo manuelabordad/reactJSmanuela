5d5737781dd4e5f3dfca4a5b3de1b595545b2328
manuelabordad <manuelaborda2019@gmail.com>
2021-07-13 12:22:41 -0500

    condicionales ternarios

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import ItemListConteiner from "./components/itemListConteiner";
+import ItemCount from "./components/ItemCount";
 
 function App() {
 	return (
 		<div>
 			<NavBar />
 			<ItemListConteiner titulo={"CATALOGO PLANTAS"} />
-			<ItemCount stock={"50"} initial={"1"} />
+			<ItemCount stock={"30"} initial={"1"} />
 		</div>
 	);
 }
